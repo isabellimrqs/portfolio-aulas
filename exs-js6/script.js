1a7d6a5d09a00eebf3ae21fd186e057842f755a2
Isabelli Maciel Marques <44590917807@>
2024-03-11 11:13:11 -0300

    add aula 6

---
+++ exs-js6/script.js
@@ -0,0 +1,148 @@
+// // // 1) Conta Bancária
+// class ContaBancaria {
+//     constructor(saldo, tipo) {
+//       this.saldo = saldo;
+//       this.tipo = tipo;
+//     }
+  
+//     sacar(valor) {
+//       if (valor > this.saldo) {
+//         console.log("Saldo insuficiente.");
+//       } else {
+//         this.saldo -= valor;
+//         console.log(`Sacou ${valor} da conta ${this.tipo}.`);
+//         console.log(`Saldo atual: ${this.saldo}`);
+//       }
+//     }
+  
+//     depositar(valor) {
+//       this.saldo += valor;
+//       console.log(`Depositou ${valor} na conta ${this.tipo}.`);
+//       console.log(`Saldo atual: ${this.saldo}`);
+//     }
+//   }
+  
+//   class ContaCorrente extends ContaBancaria {
+//     constructor(saldo) {
+//       super(saldo, "corrente");
+//     }
+//   }
+  
+//   class ContaPoupanca extends ContaBancaria {
+//     constructor(saldo) {
+//       super(saldo, "poupança");
+//     }
+  
+//     renderJuros(taxa) {
+//       const juros = this.saldo * taxa;
+//       this.depositar(juros);
+//       console.log(`Juros de ${juros} renderizados na conta ${this.tipo}.`);
+//     }
+//   }
+  
+
+//   const minhaContaCorrente = new ContaCorrente(1500);
+//   minhaContaCorrente.depositar(500);
+//   minhaContaCorrente.sacar(100);
+  
+//   const minhaContaPoupanca = new ContaPoupanca(5000);
+//   minhaContaPoupanca.depositar(100);
+//   minhaContaPoupanca.renderJuros(0.15);
+
+
+// //  // Sistema de Livros
+// class Livro {
+//     constructor(nome, autor, identificador, categoria, quantidade) {
+//       this.nome = nome;
+//       this.autor = autor;
+//       this.identificador = identificador;
+//       this.categoria = categoria;
+//       this.quantidade = quantidade;
+//       this.emprestados = 0;
+//     }
+  
+//     emprestar() {
+//       if (this.quantidade > 0) {
+//         this.quantidade--;
+//         this.emprestados++;
+//         console.log(`Livro "${this.nome}" foi emprestado.`);
+//       } else {
+//         console.log(`Não está disponível o livro: "${this.nome}".`);
+//       }
+//     }
+  
+//     devolver() {
+//       if (this.emprestados > 0) {
+//         this.quantidade++;
+//         this.emprestados--;
+//         console.log(`Livro "${this.nome}" devolvido.`);
+//       } else {
+//         console.log(`Não foi emprestado.`);
+//       }
+//     }
+//   }
+  
+//   const livro1 = new Livro("É Assim que Acaba", "Colleen Hoover", "ISBN123", "Romance", 2);
+//   const livro2 = new Livro("A Garota do Lago", "Charlie Donlea", "ISBN321", "Suspense", 3);
+  
+//   livro1.emprestar();
+//   livro1.emprestar();
+//   livro1.devolver();
+  
+//   livro2.emprestar();
+//   livro2.devolver();
+  
+
+// // // 3) Autenticação de Usuário
+// class Usuario {
+//     constructor(username, senha) {
+//       this.username = username;
+//       this.senha = senha;
+//       this.logado = false;
+//     }
+//   }
+  
+//   class Autenticacao {
+//     constructor() {
+//       this.usuarios = [];
+//     }
+  
+//     registrar(username, senha) {
+//       const novoUsuario = new Usuario(username, senha);
+//       this.usuarios.push(novoUsuario);
+//       console.log(`Usuário ${username} registrado.`);
+//     }
+  
+//     fazerLogin(username, senha) {
+//       const usuario = this.usuarios.find(user => user.username === username && user.senha === senha);
+//       if (usuario) {
+//         usuario.logado = true;
+//         console.log(`Usuário ${username} logado.`);
+//       } else {
+//         console.log(`Credenciais inválidas.`);
+//       }
+//     }
+  
+//     fazerLogout(username) {
+//       const usuario = this.usuarios.find(user => user.username === username);
+//       if (usuario) {
+//         usuario.logado = false;
+//         console.log(`Usuário ${username} deslogado.`);
+//       } else {
+//         console.log(`Usuário não encontrado.`);
+//       }
+//     }
+//   }
+
+
+
+//   const sistemaAutenticacao = new Autenticacao();
+  
+//   sistemaAutenticacao.registrar("juliana", "123");
+//   sistemaAutenticacao.registrar("isabelli", "321");
+  
+//   sistemaAutenticacao.fazerLogin("juliana", "123");
+//   sistemaAutenticacao.fazerLogin("isabelli", "senhaerrada");
+  
+//   sistemaAutenticacao.fazerLogout("juliana");
+    
